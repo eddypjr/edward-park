0f379caf2b4facce7804a9da70e4a7d534ef1983
Edward Park <edwardpark123@gmail.com>
2023-08-08 13:30:49 -0700

    configured airbnb styleguide

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/* eslint-disable import/no-extraneous-dependencies */
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import esLint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [esLint(), react()],
+});
