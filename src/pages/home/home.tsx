67e5d8b1fb78e5242db8abbede9dbb9ea6efd2c4
Edward Park <edwardpark123@gmail.com>
2023-08-08 12:11:24 -0700

    Home page created

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,13 @@
+import NavBar from '../../components/layout/nav-bar/nav-bar';
+import Banner from '../../components/banner/banner';
+
+const Home = () => {
+  return (
+    <>
+      <NavBar />
+      <Banner />
+    </>
+  );
+};
+
+export default Home;
