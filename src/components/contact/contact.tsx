6108f8faed517c8078454c62b1124be52822c856
Edward Park <edwardpark123@gmail.com>
2023-08-08 17:53:47 -0700

    pushing up work as of now bc laptop needs to reboot

---
+++ src/components/contact/contact.tsx
@@ -0,0 +1,16 @@
+import '../../styles/components/contact.scss';
+
+export default function Contact() {
+  return (
+    <section className='contact'>
+      <div className='container'>
+        <div className='content'>
+          <div className='contact-header'>
+            <h3>Contact</h3>
+            <h4>Let&apos;s get in touch!</h4>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,15 @@
 import NavBar from '../../components/layout/nav-bar/nav-bar';
 import Banner from '../../components/banner/banner';
+import AboutMe from '../../components/about-me/about-me';
+import Projects from '../../components/projects/projects';
 
 function Home() {
   return (
     <>
       <NavBar />
       <Banner />
+      <AboutMe />
+      <Projects />
     </>
   );
 }
