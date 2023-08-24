2809a978459c7c7fb41bcec9013709c0dd4e4a4e
Edward Park <edwardpark123@gmail.com>
2023-08-24 13:19:01 -0700

    changed About to Experience in NavBar

--- src/components/layout/nav-bar/nav-bar.tsx
+++ src/components/layout/nav-bar/nav-bar.tsx
@@ -1,19 +1,25 @@
+import { useState } from 'react';
 import { HashLink as Link } from 'react-router-hash-link';
+import { faBarsStaggered, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './nav-bar.scss';
 
 function NavBar() {
+  const [hamburger, setHamburger] = useState(false);
+  const hamburgerMenu = () => setHamburger(!hamburger);
+
   const navlinks = [
     {
       name: 'Home',
-      link: '#home',
+      link: '#',
     },
     {
-      name: 'About',
-      link: '#about',
+      name: 'Experience',
+      link: '#experience',
     },
     {
-      name: 'Projects',
-      link: '#projects',
+      name: 'Portfolio',
+      link: '#portfolio',
     },
     {
       name: 'Contact',
@@ -26,20 +32,39 @@ function NavBar() {
   };
 
   return (
-    <nav className='navbar'>
-      <h3 className='logo' onClick={pageUp} role='presentation' aria-label='To the top'>
-        Edward Park
-      </h3>
-      <ul>
-        {navlinks.map((navItem) => (
-          <li key={navItem.name}>
-            <Link to={navItem.link} aria-label={navItem.name}>
-              {navItem.name}
-            </Link>
+    <>
+      <nav className='navbar'>
+        <h1 className='logo' onClick={pageUp} role='presentation' aria-label='To the top'>
+          Edward Park
+        </h1>
+        <ul>
+          {navlinks.map((navItem) => (
+            <li key={navItem.name}>
+              <Link to={navItem.link} aria-label={navItem.name}>
+                {navItem.name}
+              </Link>
+            </li>
+          ))}
+          <li onClick={hamburgerMenu} aria-label='menu' role='presentation'>
+            <FontAwesomeIcon icon={faBarsStaggered} className='mobile-menu' />
           </li>
-        ))}
-      </ul>
-    </nav>
+        </ul>
+      </nav>
+
+      <div className={`mobile-nav ${hamburger ? 'open' : 'closed'}`}>
+        <span onClick={hamburgerMenu} role='presentation'>
+          <FontAwesomeIcon icon={faXmark} />
+        </span>
+
+        <ul>
+          {navlinks.map((item) => (
+            <li key={item.name} onClick={hamburgerMenu} role='presentation'>
+              <a href={item.link}>{item.name}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
