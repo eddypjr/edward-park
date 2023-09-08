7b08b3a218aa20b598d35e591f863282a16a5f26
Edward Park <edwardpark123@gmail.com>
2023-09-08 11:41:59 -0700

    added title and icon

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import esLint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [esLint(), react()],
+  plugins: [react()],
 });
